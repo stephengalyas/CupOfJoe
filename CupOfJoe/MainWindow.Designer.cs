4f9e93b91121fe526556b6ed6f6d10c95e23570e
Stephen Galyas <stephen.galyas@student.fairfield.edu>
2017-02-05 01:23:27 -0500

    Initial VS commit
    First commit with Visual Studio files. The main window contains one
    button, which sets the sleep mode on AC value to 0 (Never).

---
+++ CupOfJoe/MainWindow.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace CupOfJoe
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTest1
+            // 
+            this.btnTest1.AccessibleDescription = "Test Button 1";
+            this.btnTest1.AccessibleName = "Test Button 1";
+            this.btnTest1.Location = new System.Drawing.Point(136, 148);
+            this.btnTest1.Name = "btnTest1";
+            this.btnTest1.Size = new System.Drawing.Size(183, 51);
+            this.btnTest1.TabIndex = 0;
+            this.btnTest1.Text = "Give Me Coffee";
+            this.btnTest1.UseVisualStyleBackColor = true;
+            this.btnTest1.Click += new System.EventHandler(this.btnTest1_Click);
+            // 
+            // MainWindow
+            // 
+            this.AccessibleDescription = "The main window for CupOfJoe";
+            this.AccessibleName = "Main Window";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(458, 395);
+            this.Controls.Add(this.btnTest1);
+            this.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HelpButton = true;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Tag = "";
+            this.Text = "Cup Of Joe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest1;
+    }
+}
+
